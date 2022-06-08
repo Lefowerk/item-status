f970a092fada9b4c35f41c9b4357686857d7612e
Lefowerk <107107516+Lefowerk@users.noreply.github.com>
2022-06-08 10:52:48 +0200

    Add files via upload

---
+++ function.js
@@ -0,0 +1,27 @@
+window.function = function (re, bo, start, end) { 
+  const today = new Date()
+  today.setHours(0,0,0,0) 
+  const s = new Date(start.value)
+  s.setHours(0,0,0,0)
+  const e = new Date(end.value)
+  e.setHours(0,0,0,0)
+  
+  
+  if (s.valueOf() > today.valueOf()) {
+    return 'Reserviert'
+  } else if (!bo.value && s.valueOf() == today.valueOf()) {
+    return 'Abholung heute'
+  } else if (s.valueOf() < today.valueOf() && !bo.value) {
+    return 'Nicht abgeholt'
+  } else if(re.value) {
+    return 'Zurückgegeben'
+  } else if (bo && e.valueOf() == today.valueOf()) {
+    return 'Rückgabe heute'
+  } else if (bo && e.valueOf() < today.valueOf()) {
+    return 'Überfällig'
+  } else if (bo.value && !re.value) {
+    return 'Ausgeliehen'
+  } else {
+    return 'Fehler'
+  }
+}
